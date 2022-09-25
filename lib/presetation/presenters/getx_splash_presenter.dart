b6cd4975c00612fa8f92ce04ea87d613ec6fb2f1
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-09-25 20:56:29 -0300

    Move GetxSplashPresenter to its own file

---
+++ lib/presetation/presenters/getx_splash_presenter.dart
@@ -0,0 +1,25 @@
+import '../../domain/usecases/usecases.dart';
+import '../../ui/pages/pages.dart';
+
+import 'package:meta/meta.dart';
+import 'package:get/get.dart';
+
+class GetxSplashPresenter implements SplashPresenter{
+  final LoadCurrentAccount loadCurrentAccount;
+  var _navigateTo = RxString();
+
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+
+  Future<void> checkAccount() async{
+    try{
+      final account = await loadCurrentAccount.load();
+      _navigateTo.value = account.isNull ? "/login" : "/surveys";
+    }catch(error){
+      _navigateTo.value = "/login";
+    }
+
+  }
+}
--- test/presetation/presenters/getx_splash_presenter_test.dart
+++ test/presetation/presenters/getx_splash_presenter_test.dart
@@ -1,31 +1,12 @@
+import 'package:GreencityTeam/presetation/presenters/getx_splash_presenter.dart';
 import 'package:GreencityTeam/domain/entities/entities.dart';
-import 'package:GreencityTeam/ui/pages/pages.dart';
 import 'package:GreencityTeam/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
-import 'package:get/get.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class GetxSplashPresenter implements SplashPresenter{
-  final LoadCurrentAccount loadCurrentAccount;
-  var _navigateTo = RxString();
-
-  Stream<String> get navigateToStream => _navigateTo.stream;
 
 
-  GetxSplashPresenter({@required this.loadCurrentAccount});
 
-  Future<void> checkAccount() async{
-    try{
-      final account = await loadCurrentAccount.load();
-      _navigateTo.value = account.isNull ? "/login" : "/surveys";
-    }catch(error){
-      _navigateTo.value = "/login";
-    }
-
-  }
-}
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount{}
 
